765986c0a293f015f395a5ee712fefdcd147816a
barath3011 <barathtech30@gmail.com>
2026-02-01 16:48:41 +0530

    Fix package structure for JPA scanning

--- src/main/java/sql/demo/demo/DemoApplication.java
+++ src/main/java/sql/demo/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package sql.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("sql.demo.demo.entity")
+@EnableJpaRepositories("sql.demo.demo.repository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
