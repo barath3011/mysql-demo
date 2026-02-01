e8831e9212417a240ddef63f5a2c9b844b0de0f3
barath3011 <barathtech30@gmail.com>
2026-02-01 09:36:42 +0530

    Demo Spring Boot MySQL test

---
+++ src/test/java/sql/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sql.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
